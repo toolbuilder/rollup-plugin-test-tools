58253f56b07ad1147c26a119c89958359439637d
toolbuilder <k_d_hudson@yahoo.com>
2022-01-06 08:22:27 -0500

    feat: export named Rollup test configurations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as createTestPackageJson } from 'rollup-plugin-create-test-package-json'
+export { default as multiEntry } from '@rollup/plugin-multi-entry'
 export { default as multiInput } from 'rollup-plugin-multi-input'
 export { default as relativeToPackage } from 'rollup-plugin-relative-to-package'
 export { default as createPackFile } from '@toolbuilder/rollup-plugin-create-pack-file'
@@ -8,5 +9,9 @@ export { basePackfileTestConfig } from './node-configs.js'
 
 import { browserTestConfig } from './browser-config.js'
 import { nodeConfigs } from './node-configs.js'
+
+export const nodeEsTestConfig = nodeConfigs[0]
+export const nodeCjsTestConfig = nodeConfigs[1]
+export { browserTestConfig }
 
 export const testConfigs = [...nodeConfigs, ...browserTestConfig]
