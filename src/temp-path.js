2a85d653e407a5e1c41d7b51441b5ef6f3358145
toolbuilder <k_d_hudson@yahoo.com>
2023-08-12 13:51:09 -0400

    feat: change from cuid to nanoid because cuid deprecated

--- src/temp-path.js
+++ src/temp-path.js
@@ -1,7 +1,10 @@
-import cuid from 'cuid'
 import { format } from 'date-fns'
+import { customAlphabet } from 'nanoid'
 import { tmpdir } from 'os'
 import { join } from 'path'
+
+// make a unique number so that each Rollup run produces a different temporary directory
+const nanoid = customAlphabet('1234567890abcdef', 10)
 
 /**
  * Creates a unique temporary path.
@@ -15,5 +18,5 @@ import { join } from 'path'
 export const tempPath = (prefix = 'package-test') => {
   // formatISO emits colons for the time part, which can be problematic on command lines as NPM parameters
   const timePart = format(Date.now(), "yyyy-MM-dd'T'kk-mm")
-  return join(tmpdir(), `${prefix}-${timePart}-${cuid.slug()}`) // slug provides uniqueness in same minute
+  return join(tmpdir(), `${prefix}-${timePart}-${nanoid()}`)
 }
